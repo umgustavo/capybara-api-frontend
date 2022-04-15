4fd826746622030fde62797fb49d5d2daad9b616
Gustavo <gugu.r.azevedo@gmail.com>
2022-04-15 20:03:17 -0300

    Remove system color mode

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,6 +3,8 @@ import colors from './colors';
 
 const theme = extendTheme({
     colors: colors,
+    initialColorMode: 'light',
+    useSystemColorMode: false,
     fonts: {
         body: '"DM Sans", sans-serif',
         heading: '"DM Sans", serif',
